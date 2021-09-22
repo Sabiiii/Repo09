549a81e4b8587068a5b180ccc9c80e0c15f08129
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-22 10:57:06 +0400

    Adding switch statements

---
+++ ex11/src/index.js
@@ -0,0 +1,29 @@
+function switchCase(letter) {
+
+   switch (letter) {
+
+     case "a": 
+          a ("antelope");
+          break;
+     case "b":
+          b ("bird");
+          break;
+     case "c":
+          c ("cat");
+          break;
+     default: 
+          d = ("stuff");
+          break;
+
+   }
+    
+return animals;
+}
+
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
+
+module.exports = switchCase; 
