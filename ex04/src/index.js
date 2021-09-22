cdaebe5f79d403e3ba046ba95289174674c2f19f
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-22 07:51:10 +0400

    Adding comparing different values

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,4 +1,21 @@
 function compareDifferentValues(m, n) {
 
-    
+    var num1 = [8, "8", '8', "8", 8];
+    var num2 = ["8", 8, 8, "8", 8];
+
+    if (m === n) {
+        return "Equal"; 
+    } 
+
+    return "Not Equal";
+
 }
+
+
+console.log (compareDifferentValues(8, "8"));
+console.log (compareDifferentValues("8", 8));
+console.log (compareDifferentValues('8', 8));
+console.log (compareDifferentValues("8", "8"));
+console.log (compareDifferentValues(8, 8));
+
+ module.exports = compareDifferentValues;
