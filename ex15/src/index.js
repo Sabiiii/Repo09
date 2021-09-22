9caf9caa6c079457d273572b39205c12ce5cc7e2
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-22 12:54:45 +0400

    Adding do whiile loops

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,6 +1,15 @@
 function myDoWhile() { 
 
-    var myNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+    var myNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+    
+    do {
+        console.log(myNumbers);
+        i++;
+
+    } while (myNumbers < 9);
+
 }
+
+console.log(myDoWhile());
 
 module.exports = myDoWhile;
