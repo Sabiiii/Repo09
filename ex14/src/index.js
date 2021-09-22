d103024e2169c4f29c590667143bcf9a80e11bad
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-22 11:51:07 +0400

    Adding for loops

---
+++ ex14/src/index.js
@@ -0,0 +1,29 @@
+function myForLoop1() {
+
+    var evenNumbers = [0, 2, 4, 6, 8];
+
+    for (var evenNumbers = 0; evenNumbers < 10; i++)
+    console.log(evenNumbers);
+
+ return evenNumbers;
+}
+
+function myForLoop2() {
+
+    var evenInverseNumbers = [8, 6, 4, 2, 0];
+
+    for (var evenInverseNumbers = 8; evenInverseNumbers > 0; i--)
+    console.log(evenInverseNumbers);
+
+    return evenInverseNumbers;
+
+
+}
+
+console.log(myForLoop1());
+console.log(myForLoop2());
+
+module.exports = {
+    myForLoop1,
+    myForLoop2
+};
